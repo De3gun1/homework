0471aecce18972223f3ebac0c3a5b566ed4ec9bd
Matvey Vasilyuk <MV@gmail.com>
2025-07-26 18:14:14 +0300

    Saying bye

---
+++ file1.py
@@ -0,0 +1,21 @@
+# Unfinished
+print("bye1")
+
+# Unfinished
+print("bye2")
+
+
+# Unfinished
+print("bye3")
+
+
+# Done
+print("hello")
+
+
+# Unfinished
+print("bye")
+
+
+# Unfinished
+print("bye")
